2177e1da859870e804fc241a545912d0331e076f
Prince Modi <princebmodi@gmail.com>
2023-02-26 19:33:15 +0530

    Implemented/Imported track.py

--- depth_yolo.py
+++ depth_yolo.py
@@ -1,16 +1,21 @@
 #pseudo code
 import iros_utils as iu
 import extract_frames as ef
+from track import load_models
 import cv2
 
+yolo,vest=load_models()
+
 i = 0
-while True:
+while 0:
     frame = ef.fetch_frame(i)
     cv2.imshow("frame", frame)
     if cv2.waitKey(1) & 0xFF == ord('q'):
         cv2.destroyAllWindows()
         break
     i += 1
+
+
 
 # frame = iu.fetch_frame()
 # bboxes = yolo(frame)
