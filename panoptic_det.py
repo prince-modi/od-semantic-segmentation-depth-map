1375fd332d583c3c2b6c10335a7821f2156d5654
Prince Modi <princemodi@iisc.ac.in>
2023-03-05 15:04:37 +0530

    For Ruchi

--- panoptic_det.py
+++ panoptic_det.py
@@ -40,43 +40,52 @@ try:
         outputs, segments_info = predictor(frame)["panoptic_seg"]
         print(1)
         res=time.time()
-        # try:
-        #     # print("tryyy")
-        #     # segs = [i['id'] for i in segments_info if 'category_id' in i and i['category_id']==21]# or i['category_id']==44)]
-        #     # print(len(segs))
-        #     segs2 = [i['id'] for i in segments_info if 'category_id' in i and i['category_id']==21]# road or pavement
-        #     print(len(segs2))
-        # except Exception as ee:
-        #     continue
+        try:
+            print("tryyy")
+            segs = [i['id'] for i in segments_info if 'category_id' in i and i['category_id']==21]# or i['category_id']==44)]
+            # print(len(segs))
+            segs2 = [i['id'] for i in segments_info if 'category_id' in i and i['category_id']==44]# road or pavement
+            print(len(segs2))
+        except Exception as ee:
+            continue
         # finally:
         #     i+=1
-        # # outputs1=outputs.clone().detach()
-        # outputs2=outputs.clone().detach()
+        outputs1=outputs.clone().detach()
+        outputs2=outputs.clone().detach()
         # t2=time.time()
-        mask = [i['id'] for i in segments_info if i['isthing']==True]
+        # try:
+        #     mask = [i['id'] for i in segments_info if i['isthing']==True]
+        #     outputs = out
+        # # a = a[~np.isin(a, elements_to_remove)]
+        # except Exception as ee:
+        #     continue
+        
         # print(mask)
         # outputs[outputs not in mask]=0
         v = Visualizer(frame[:, :, ::-1], MetadataCatalog.get(cfg.DATASETS.TRAIN[0]), scale=1.2)
-        # # if len(segs):
-        # #     outputs1[outputs1!=segs[0]]=0
-        # if len(segs2):
-        #     outputs2[outputs2!=segs2[0]]=0
-        # # if len(segs) and not len(segs2):
-        # #     finaloutput=outputs1
-        # # if len(segs2) and not len(segs):
-        # #     finaloutput=outputs2
-        # # finaloutput=outputs1+outputs2
+        if len(segs):
+            outputs1[outputs1!=segs[0]]=0
+        if len(segs2):
+            outputs2[outputs2!=segs2[0]]=0
+        if len(segs) and not len(segs2):
+            finaloutput=outputs1
+        if len(segs2) and not len(segs):
+            finaloutput=outputs2
+        finaloutput=outputs1+outputs2
         # finaloutput=outputs2
 
         # with open('output.txt','w') as f:
         #     f.write(str(segments_info))
-
-        out = v.draw_panoptic_seg_predictions(outputs.to("cpu"), segments_info)
-        t3 = time.time()
-        frame = out.get_image()[:, :, ::-1]
-        cv2.imshow("frame", frame)
-        cv2.waitKey(1)
-        cv2.imwrite(f'finale/sem_seg_lane/{i}.jpg', frame) 
+        try:
+            out = v.draw_panoptic_seg_predictions(finaloutput.to("cpu"), segments_info)
+            t3 = time.time()
+            frame = out.get_image()[:, :, ::-1]
+            cv2.imshow("frame", frame)
+            cv2.waitKey(1)
+            cv2.imwrite(f'finale/sem_seg_lane/{i}.jpg', frame) 
+        except Exception as pe:
+            print(pe)
+            continue
         i=i+1
         # print(f'loop: {time.time()-t0}, preprocessing: {t1-t0}, inferencing: {t2-t1}, overhead: {t3-t2}')
         inf_row.append(res-t1)
